e702ca6ebbed76b83e224f26e4fab21bf6b3bb45
Joey McQuade <joeymmcquade@outlook.com>
2023-07-04 15:11:04 +0100

    Feature: Turned into function

--- ExcelData.py
+++ ExcelData.py
@@ -5,10 +5,14 @@ df = pd.read_excel(SECRET_PATH, engine='openpyxl')
 
 profile_list = df.values.tolist()
 profile_set = set(df['Profile'])
-for value in profile_set:
+
+def profile_dict_constructor(profile_list: list, profile_set: set, value: int) -> dict:
     profile_dict = {value: {'X':[], 'Chainage': []}}
     for point_profile in profile_list:
         if point_profile[0] == list(profile_dict.keys())[0]:
             profile_dict[value]['X'].append(point_profile[3])
             profile_dict[value]['Chainage'].append(point_profile[5])
-    print(profile_dict)
+    return profile_dict
+
+for value in profile_set:
+    print(profile_dict_constructor(profile_list, profile_set, value))
